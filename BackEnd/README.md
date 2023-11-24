ed3fd648466b51fcb68529d56d84663decf2b779
Prakhar Singh <singhprakhar359@gmail.com>
2023-11-24 16:24:55 +0530

    first Commit

---
+++ BackEnd/README.md
@@ -0,0 +1,12 @@
+# Dev-Folio
+
+## Web Programming Project
+
+
+### Technologies Used
+
+- [MongoDB](https://www.mongodb.com/)
+- [ExpressJS](https://expressjs.com/)
+- [ReactJS](https://reactjs.org/)
+- [NodeJS](https://nodejs.org/en/)
+
---
+++ BackEnd/db.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+
+// Connection to MongoDB
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      // useCreateIndex: true,
+      // useFindAndModify: false,
+    });
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    console.error(err.message);
+    // Exit process with failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
